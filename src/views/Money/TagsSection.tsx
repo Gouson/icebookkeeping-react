509d86a985c0ed1080ff428d6c99a660f6774ced
Gouson <gouson@qq.com>
2021-04-09 14:46:25 +0800

    TagSection功能

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,6 @@
+import { useState } from "react"
 import styled from "styled-components"
-
-const TagsSection = styled.section`
+const Wrapper = styled.section`
     background:#ffffff;
     padding:12px 16px;
     flex-grow:1;
@@ -17,6 +17,10 @@ const TagsSection = styled.section`
             padding:3px 18px;
             font-size:14px; 
             margin:8px 12px; 
+            &.selected{
+                background:#5161C4;
+                color:#ffffff;
+            }
         }
     }
     >button{
@@ -27,5 +31,36 @@ const TagsSection = styled.section`
         margin-top:8px;
     }
 `
+const TagsSection: React.FC = (props) => {
+    const [tags, setTags] = useState<string[]>(['衣', '11', '食', '住', '行'])
+    const [selectedTags, setSelectedTags] = useState<string[]>([])
+    const onAddTag = () => {
+        const tagName = window.prompt('新标签为')
+        if (tagName !== null) {
+            setTags([...tags, tagName])
+        }
+    }
 
-export {TagsSection}
+    const onToggleTag = (tag: string) => {
+        const index = selectedTags?.indexOf(tag)
+        if (index >= 0) {
+            //如果tag已被选中，九福之所有没有被选中的tag，做微信的selectedTag
+            setSelectedTags(selectedTags.filter(t => t !== tag))
+        } else {
+            setSelectedTags([...selectedTags, tag])
+        }
+    }
+
+    const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : ''
+    return (
+        <Wrapper>
+            <ol>
+                {tags.map(tag =>
+                    <li key={tag} onClick={() => { onToggleTag(tag) }} className={getClass(tag)}>{tag}</li>
+                )}
+            </ol>
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
+    )
+}
+export { TagsSection }

0ca588f7a5e3419b9a7c8229f464f4d9c84ce15b
Gouson <gouson@qq.com>
2021-04-16 18:26:01 +0800

    useRecords

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,35 +5,42 @@ import { CategorySection } from './Money/CategorySection';
 import { NumberPadSection } from './Money/NumberPadSection';
 import { TagsSection } from './Money/TagsSection';
 import { useState } from 'react';
+import { useRecords } from 'hooks/useRecords';
 
 const MyLayout = styled(Layout)`
    display: flex;
    flex-direction:column;
 `
 type Category = '-' | '+'
+
+const defaultFormData = {
+    tagIds: [] as number[],
+    note: '',
+    category: '-' as Category,
+    amount: 0
+}
+
 function Money() {
-    const [selected, setSelected] = useState({
-        tagIds: [] as number[],
-        note: '',
-        category: '-' as Category,
-        amount: 0
-    })
+    const [selected, setSelected] = useState(defaultFormData)
     type Selected = typeof selected;
     const onChange = (obj: Partial<Selected>) => {
         setSelected({ ...selected, ...obj })
     }
 
+    const { records, addRecord } = useRecords()
     const submit = () => {
-        console.log(selected)
-        
+        addRecord(selected)
+        alert('保存成功')
+        setSelected(defaultFormData)
     }
     return (
+
         <MyLayout >
             <TagsSection value={selected.tagIds} onChange={tagIds => onChange({ tagIds })} />
             <NoteSection value={selected.note} onChange={note => onChange({ note })} />
             <CategorySection value={selected.category} onChange={category => onChange({ category })} />
             <NumberPadSection value={selected.amount} onChange={amount => onChange({ amount })} onOk={submit} />
-        </MyLayout>
+        </MyLayout >
     );
 }
 export default Money

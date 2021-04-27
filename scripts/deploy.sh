8c047fd597cac818191c5a196e99a0b7614adf03
Gouson <gouson@qq.com>
2021-04-27 15:22:53 +0800

    dep

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ git init &&
 git add . &&
 git commit -m'deploy' &&
 git remote add origin https://github.com/Gouson/rainbow-bookkeeping.git &&
+#git remote add origin https://gitee.com/gaokexin/rainbow-bookkeeping.git &&
 git branch -M main &&
 git push -u origin main -f &&
 cd -
